afce052839d2502d5cab650b6bac02d9451481a8
evelyn howell <howellevelyn00@gmail.com>
2025-09-19 18:12:30 -0600

    prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your First Name?");
+        string firstName = Console.ReadLine();
+
+        Console.WriteLine("What is your Last Name?");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine($"My name is {lastName}, {firstName} {lastName}.");
     }
 }
