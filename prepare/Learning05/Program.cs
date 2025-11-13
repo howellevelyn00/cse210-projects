c9eacce46358a664cd64a0a0d0b037a95626e3bc
evelyn howell <howellevelyn00@gmail.com>
2025-11-13 16:45:37 -0700

    Shapes

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,23 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        // Build a list of different shapes
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Square("Red", 4));
+        shapes.Add(new Rectangle("Blue", 5, 3));
+        shapes.Add(new Circle("Green", 2));
+
+        // Show color + area for each
+        foreach (Shape s in shapes)
+        {
+            Console.WriteLine("Color: " + s.GetColor());
+            Console.WriteLine("Area: " + s.GetArea());
+            Console.WriteLine();
+        }
     }
 }
